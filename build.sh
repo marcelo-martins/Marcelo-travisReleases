8d7f635f92fb48f850f1cbb0050f963d03c66150
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-10 22:01:18 -0300

    b

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest-$github_versions -o /dev/null)
 
-if [  $status == 404 ] 
+if [ $status == 404 ] 
 then
     echo "CLONING"
     git clone https://github.com/restic/restic/
@@ -14,10 +14,8 @@ then
     echo "RESTIC VERSION"
     - ./restic version
     
-    #echo "TESTING"
-    #- go test ./...
-    
     echo "MOVING BINARY"
+    mv restic restic-$github_version
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version" 
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest restic-$ftp_version" 
 fi
