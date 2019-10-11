7197d55522fa74e14731da7773ab11d03c1cc1ce
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-10 23:42:33 -0300

    probably the last try today, changed the lftp commands

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+  
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
+del_version=$(cat delete_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest/restic-$github_version -o /dev/null)
 
-if [ $status == 404 ] 
+if [$github_version != $ftp_version ] 
 then
     echo "CLONING"
     git clone https://github.com/restic/restic/
@@ -12,10 +14,11 @@ then
     go run build.go -T
     
     echo "MOVING BINARY"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; mv restic-$ftp_version ../"
-    mv restic restic-$github_version
-    
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version" 
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; put restic-$github_version"
+    if [[ $github_version > $ftp_version ]]
+    then
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version"
+    fi
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic restic-$github_version"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/restic-$del_version"
 fi
