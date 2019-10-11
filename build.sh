f4eec7c1657bf436d4ddc10928c185577af04a8b
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-10 22:49:25 -0300

    changed the way it mv and rm from ftp on build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,10 @@ then
     go run build.go -T
     
     echo "MOVING BINARY"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; mv restic-$ftp_version ../"
     mv restic restic-$github_version
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version" 
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
+    
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version" 
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; put restic-$github_version"
 fi
