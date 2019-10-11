4d7e767e8847dc9213df04950f74e6d33e927a11
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-10 23:10:34 -0300

    trying to figure out the index

--- build.sh
+++ build.sh
@@ -2,23 +2,20 @@ github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest/restic-$github_version -o /dev/null)
 
-addr="ftp://oplab9.parqtec.unicamp.br"
-path="test/marcelo/restic/latest/"
-
-if [  $status == 404 ] 
+if [ $status == 404 ] 
 then
-    echo "GET LATEST REPO"
+    echo "CLONING"
     git clone https://github.com/restic/restic/
     cd restic
-    git checkout tags/v$github_version
     
-    echo "BUILD AND TEST"
+    echo "BUILDING"
     go run build.go -T
     
-    echo "MOVE OLD FROM LATEST"
-    lftp -c "open -u $USER,$PASS $addr; cd $path; mv restic-$ftp_version ../"
-
-    echo "SEND BINARY"
+    echo "MOVING BINARY"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; mv restic-$ftp_version ../"
     mv restic restic-$github_version
-    lftp -c "open -u $USER,$PASS $addr; cd $path; put restic-$github_version"
+    
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version" 
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; cd test/marcelo/restic/latest/; put restic-$github_version"
 fi
--- ftp_version.py
+++ ftp_version.py
@@ -1,25 +1,23 @@
-import requests as res
-
+import requests
 # find and save the current Github release
 html = str(
-    res.get('https://github.com/restic/restic/releases/latest')
-    .content
-)
-i = html.find('Release')
-github_version = html[i:i+20].split()[2]
+           requests.get('https://github.com/restic/restic/releases/latest')
+           .content)
+index = html.find('Release')
+print(html)
+print(str(i))
+github_version = html[index:index+20].split()[2]
 file = open('github_version.txt', 'w')
 file.writelines(github_version)
 file.close()
-print("Latest: ", github_version)
 
+# find and save the current Docker version on FTP server
 html = str(
-    res.get(
-    'https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest'
-    ).content
-)
-i = html.find('restic-')
-ftp_version = html[i:i+20].split('-')[1].split('"')[0]
+             requests.get(
+                        'https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest'
+                        ).content)
+index = html.find('restic-')
+ftp_version = html[index:index+20].split('-')[1].split('"')[0]
 file = open('ftp_version.txt', 'w')
 file.writelines(ftp_version)
 file.close()
-print("Current: ", ftp_version)
