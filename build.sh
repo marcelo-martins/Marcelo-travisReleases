51766d90eed5056df3004df4b9e677f3e6688d11
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-14 13:33:11 -0300

    trying other ways to upload to ftp

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest/restic-$github_version -o /dev/null)
 
+addr="ftp://oplab9.parqtec.unicamp.br"
+path="test/marcelo/restic/latest/"
+
+
 if [$github_version != $ftp_version ] 
 then
     echo "CLONING"
@@ -14,12 +18,17 @@ then
     go run build.go -T
     mv restic restic-$github_version
     
-    echo "MOVING BINARY"
-    if [[ $github_version > $ftp_version ]]
-    then
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version"
-    fi
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic restic-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/restic-$del_version"
+    # echo "MOVING BINARY"
+    # if [[ $github_version > $ftp_version ]]
+    # then
+    #     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
+    #     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version"
+    # fi
+    # lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic restic-$github_version"
+    # lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/restic-$del_version"
+
+    lftp -c "open -u $USER,$PASS $addr; cd $path; mv restic-$ftp_version ../"
+
+    mv restic restic-$github_version
+    lftp -c "open -u $USER,$PASS $addr; cd $path; put restic-$github_version"
 fi
