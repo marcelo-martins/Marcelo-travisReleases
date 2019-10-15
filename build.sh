8038775d63ded383c4c3dfe40bbaacc0a8f0bf4e
Marcelo Biagi Martins <ra183303@morais.lab.ic.unicamp.br>
2019-10-15 17:34:38 -0300

    added cotation marks on if clause

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest/restic-$github_version -o /dev/null)
  
-if [[$status -eq 404]]
+if [$status -eq "404"]
 then
     echo "CLONING"
     git clone https://github.com/restic/restic/
