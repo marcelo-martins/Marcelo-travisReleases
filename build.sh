80bcd85d3be1c9a45df51f302537a0d196092f41
Marcelo Biagi Martins <ra183303@morais.lab.ic.unicamp.br>
2019-10-15 16:56:14 -0300

    remove every delete op

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
-del_version=$(cat delete_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/test/marcelo/restic/latest/restic-$github_version -o /dev/null)
-
-addr="ftp://oplab9.parqtec.unicamp.br"
-path="/test/marcelo/restic/latest/"
-
-   
-if [ [$status==404] ] #talvez um ponto e virgula aqui
+ 
+if [[$status==404]]
 then
     echo "CLONING"
     git clone https://github.com/restic/restic/
@@ -18,16 +13,10 @@ then
     mv restic restic-$github_version
     
     echo "MOVING BINARY"
+
     if [[ $github_version > $ftp_version ]]
     then
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic/latest restic-$github_version"
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/latest/restic-$ftp_version"
     fi
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/restic restic-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/restic/restic-$del_version"
-
-    # lftp -c "open -u $USER,$PASS $addr; cd $path; mv restic-$ftp_version ../"
-
-    # mv restic restic-$github_version
-    # lftp -c "open -u $USER,$PASS $addr; cd $path; put restic-$github_version"
 fi
